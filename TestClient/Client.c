b2f24cb75bb742f59820f34d074017adf0283043
이길복 <pst1302@gmail.com>
2015-08-19 21:31:58 +0900

    멀티 스레딩 채팅 클라이언

--- TestClient/Client.c
+++ TestClient/Client.c
@@ -1,22 +1,28 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<WinSock2.h>
+#include<string.h>
+#include<Windows.h>
+#include<process.h>
 
-#define BUF_SIZE 1024
+#define BUF_SIZE 100
+#define NAME_SIZE 20
 
+unsigned WINAPI SendMsg(void* arg);
+unsigned WINAPI RecvMsg(void* arg);
 void ErrorHandling(char* message);
+
+char name[NAME_SIZE] = "[DEFAULT]";
+char msg[BUF_SIZE];
 
 int main(int argc, char* argv[]) {
 
 	WSADATA wsaData;
-	SOCKET hSocket;
+	SOCKET hSock;
 	SOCKADDR_IN servAddr;
-
-	char message[30];
-	int strLen;
-
-	if (argc != 3) {
-		printf("Usage : %s <ip> <port> \n", argv[0]);
+	HANDLE hSndThread, hRcvThread;
+	
+	if (argc != 4) {
+		printf("Usage : %s <ip> <port> <name>\n", argv[0]);
 		getchar();
 		exit(1);
 	}
@@ -25,9 +31,12 @@ int main(int argc, char* argv[]) {
 		ErrorHandling("WSAStartup() error!");
 	}
 
+
+	sprintf_s(name,sizeof(name), "[%s]", argv[3]);
+	
 	// 소켓 생성
-	hSocket = socket(PF_INET, SOCK_STREAM, 0);
-	if (hSocket == INVALID_SOCKET) {
+	hSock = socket(PF_INET, SOCK_STREAM, 0);
+	if (hSock == INVALID_SOCKET) {
 		ErrorHandling("socket() error!");
 	}
 
@@ -38,26 +47,18 @@ int main(int argc, char* argv[]) {
 	servAddr.sin_port = htons(atoi(argv[2]));
 
 	// 서버 소켓에 연결
-	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) {
+	if (connect(hSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) {
 		ErrorHandling("connect() error!");
 	}
 
-	// message 계속 보냄
-	while (1) {
-		fputs("Input message(Q to quit) : ", stdout);
-		fgets(message, BUF_SIZE, stdin);
+	hSndThread = (HANDLE)_beginthreadex(NULL, 0, SendMsg, (void*)&hSock, 0, NULL);
+	hRcvThread = (HANDLE)_beginthreadex(NULL, 0, RecvMsg, (void*)&hSock, 0, NULL);
 
-		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
-			break;
-
-		send(hSocket, message, strlen(message), 0);
-		strLen = recv(hSocket, message, BUF_SIZE - 1, 0);
-		message[strLen] = 0;
-		printf("Message from server : %s", message);
-	}
+	WaitForSingleObject(hSndThread, INFINITE);
+	WaitForSingleObject(hRcvThread, INFINITE);
 
 	// 소켓 종료
-	closesocket(hSocket);
+	closesocket(hSock);
 	WSACleanup();
 
 
@@ -70,4 +71,41 @@ void ErrorHandling(char* message) {
 	fputs(message, stderr);
 	fputc('\n', stderr);
 	exit(1);
+}
+
+// 보내는 쓰레드 Main 함수
+unsigned WINAPI SendMsg(void* arg) {
+	SOCKET hSock = *((SOCKET*)arg);
+	char nameMsg[NAME_SIZE + BUF_SIZE];
+
+	while (1) {
+
+		fgets(msg, BUF_SIZE, stdin);
+		if (!strcmp(msg, "q\n") || !strcmp(msg, "Q\n"))
+		{
+			closesocket(hSock);
+			exit(0);
+		}
+		sprintf_s(nameMsg, sizeof(nameMsg), "%s %s", name, msg);
+		send(hSock, nameMsg, strlen(nameMsg), 0);
+	}
+
+	return 0;
+}
+
+// 받는 쓰레드 Main 함수
+unsigned WINAPI RecvMsg(void* arg) {
+	int hSock = *((SOCKET*)arg);
+	char nameMsg[NAME_SIZE + BUF_SIZE];
+	int strLen;
+
+	while (1){
+		strLen = recv(hSock, nameMsg, NAME_SIZE + BUF_SIZE - 1, 0);
+
+		if (strLen == -1)
+			return 0;
+
+		nameMsg[strLen] = 0;
+		fputs(nameMsg, stdout);
+	}
 }
